04a64548a77ffcf2d8234eaf85e704d66165f75f
Illia Kyselov <doncerber@gmail.com>
2020-09-16 01:12:59 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,120 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <div class="input__form">
+      <input
+          placeholder="Hit ENTER to add task..."
+          v-model="newTask"
+          type="text"
+          name="text"
+          id="text"
+          @keydown.enter="add"
+      />
+    </div>
+    <div class="list">
+      <app-list-item
+          @deleting="del($event)"
+          @disable="makeDisabled"
+          v-for="(item, index) in array"
+          :text="item.info"
+          :key="index"
+          :is-disabled="item.isDisabled"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Item from '@/components/Item'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      newTask: '',
+      array: JSON.parse(localStorage.getItem('arr')) || []
+    }
+  },
+  methods: {
+    add: function () {
+      if( this.newTask.trim() === '' ) return
+      this.array.push({
+        info: this.newTask.trim(),
+        isDisabled: false
+      })
+      this.newTask = ''
+      localStorage.setItem('arr', JSON.stringify(this.array))
+    },
+    makeDisabled: function (e) {
+      this.iterateArray(el => {
+        if (e === el.info) {
+          el.isDisabled = !el.isDisabled
+        }
+      })
+    },
+    del: function (e) {
+      this.iterateArray((el, index) => {
+        if (e === el.info) {
+          this.array.splice(index, 1)
+        }
+      })
+    },
+    iterateArray: function (callback) {
+      this.array.forEach(callback)
+      localStorage.setItem('arr', JSON.stringify(this.array))
+    }
+  },
   components: {
-    HelloWorld
+    'app-list-item': Item,
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap');
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .input__form {
+    margin-bottom: 20px;
+    width: 600px;
+  }
+
+  input {
+    width: 400px;
+    height: 50px;
+    font-family: 'Montserrat';
+    font-size: 1.5rem;
+    width: 100% ;
+    padding: 15px 30px;
+    border: none;
+    border-bottom: 3px solid #949494;
+
+  }
+
+  input:hover, input:focus {
+    outline: none;
+    border-bottom: 3px solid #262626;
+  }
+
+  button {
+    width: 400px;
+    height: 50px;
+    font-family: 'Montserrat';
+    border-radius: 10px;
+    font-size: 1.5rem;
+    width: 19%;
+    background-color: #4c0b94;
+    color: white;
+  }
 </style>
---
+++ src/components/Item.vue
@@ -0,0 +1,117 @@
+<template>
+  <div
+      class="contain"
+      :class="{'disabled': isDisabled}"
+  >
+    <input
+        @change="dis($event)"
+        type="checkbox"
+        name="check"
+        id="check"
+        :checked="isDisabled"
+    />
+    <div
+        class="content"
+    >
+      <p>{{ text }}</p>
+      <span
+          @click="remove($event)"
+          class="material-icons span i"
+      >delete</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Item',
+  props: {
+    text: {
+      type: String,
+      required: true
+    },
+    isDisabled: {
+      type: Boolean,
+      required: true
+    }
+  },
+  methods: {
+    remove: function (e) {
+      this.$emit('deleting', e.target.previousElementSibling.textContent)
+    },
+    dis: function (e) {
+      e.target.parentElement.classList.toggle('disabled')
+      this.$emit('disable', e.target.nextElementSibling.firstElementChild.textContent)
+    }
+  }
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap');
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+  .contain {
+    display: flex;
+    background: #bdd6ee;
+    color: #262626;
+    justify-content: space-between;
+    align-items: center;
+    width: 600px;
+    margin-bottom: 10px;
+    padding: 15px;
+    box-shadow: 0 0 0;
+    transition: box-shadow .2s linear;
+  }
+
+  .contain:hover {
+    background: #95c2ec;
+    box-shadow: 2px 4px 25px -14px rgba(0,0,0,0.55);
+  }
+
+  .content {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+  }
+
+  p {
+    margin-left: 20px;
+    font-size: 1.5rem;
+    font-family: 'Montserrat';
+  }
+
+  span {
+    margin-right: 20px;
+    cursor: pointer;
+    color: #949494;
+  }
+
+  span:hover {
+    color: #262626;
+  }
+
+  span:active {
+    color: #b00505;
+  }
+
+  input {
+    display: block;
+  }
+
+  .disabled {
+    background: #ccc;
+    color: darkgray;
+  }
+
+  .disabled:hover {
+    background: #ccc;
+    box-shadow: 0 0 0;
+  }
+</style>
